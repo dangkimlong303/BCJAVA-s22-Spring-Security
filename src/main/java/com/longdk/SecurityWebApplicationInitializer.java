cc12ad0d504dfd478e5f166acb2ac161c075b953
dangkimlong303 <dangkimlong303@gmail.com>
2019-01-07 12:03:04 +0700

    commit

---
+++ src/main/java/com/longdk/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.longdk;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
